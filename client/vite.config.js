a6d6d7e6d8e1a2596c05b57735659e4752104ff4
gisung3253 <gisung3253@naver.com>
2025-03-13 00:38:19 +0900

    전체 테스트

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // 폰으로 테스트 하는용도도
+  server: {
+    host: true,  // 외부 IP에서도 접속 가능하게 설정
+    port: 5173,  // 원하는 포트 (기본 5173)
+  },
 })
